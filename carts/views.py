651124269b066fa97f0f774bfd334b732b67503b
Elienock <lubayaelienock@gmail.com>
2022-09-17 16:30:34 +0200

    Product Variation Fetch Dynamic Color

--- carts/views.py
+++ carts/views.py
@@ -14,9 +14,9 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     color = request.GET['color']
-    size = request.GET['size']
-    return HttpResponse(color + ' ' + size)
-    exit() 
+    # size = request.GET['size']
+    return HttpResponse(color)
+    exit()
 
     product = Product.objects.get(id=product_id) # get the product_id
     try:
