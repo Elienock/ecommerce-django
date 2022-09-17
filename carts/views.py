eb8cf7fab868d01034a08c1af7dc4083659f832c
Elienock <lubayaelienock@gmail.com>
2022-09-17 15:40:20 +0200

    Product Variation Preparation

--- carts/views.py
+++ carts/views.py
@@ -13,6 +13,11 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, product_id):
+    color = request.GET['color']
+    size = request.GET['size']
+    return HttpResponse(color + ' ' + size)
+    exit() 
+
     product = Product.objects.get(id=product_id) # get the product_id
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart id present in the sesssion
